8759bb2433b2ac384b34395d1810e9062824294b
Dani <dani.code.camp@gmail.com>
2018-11-27 23:00:28 -0500

    Add files via upload

---
+++ build/precache-manifest.a1ccb8f2e3313d6f2900e9064560bd5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "835eb1b488e9065c66b8",
+    "url": "/static/js/main.835eb1b4.chunk.js"
+  },
+  {
+    "revision": "07861e9d5e216d6b0c3e",
+    "url": "/static/js/1.07861e9d.chunk.js"
+  },
+  {
+    "revision": "835eb1b488e9065c66b8",
+    "url": "/static/css/main.eb94ba85.chunk.css"
+  },
+  {
+    "revision": "e000774769c58535d995fc953f3ee815",
+    "url": "/index.html"
+  }
+];
